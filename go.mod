f7c8c613f90a378b3d91fea025ba75ac257a408c
Suraj Gupta <suraj.gupta.cse14@iitbhu.ac.in>
2025-05-06 21:53:34 +0000

    1a

---
+++ go.mod
@@ -0,0 +1,3 @@
+module codeforces-tut
+
+go 1.24.1
--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"codeforces-tut/segtree/part1/a"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	s, _ := reader.ReadString('\n')
+	parts := strings.Fields(s)
+	n, _ := strconv.Atoi(parts[0])
+	m, _ := strconv.Atoi(parts[1])
+
+	nums := make([]int, n)
+	s, _ = reader.ReadString('\n')
+	parts = strings.Fields(s)
+
+	for i := range nums {
+		nums[i], _ = strconv.Atoi(parts[i])
+	}
+
+	st := a.NewSegTree(nums)
+
+	for range m {
+		s, _ = reader.ReadString('\n')
+		parts = strings.Fields(s)
+		if parts[0] == "1" {
+			i, _ := strconv.Atoi(parts[1])
+			v, _ := strconv.Atoi(parts[2])
+
+			st.Update(i, v)
+		} else {
+			l, _ := strconv.Atoi(parts[1])
+			r, _ := strconv.Atoi(parts[2])
+
+			fmt.Fprintln(writer, st.Query(l, r))
+		}
+	}
 }
